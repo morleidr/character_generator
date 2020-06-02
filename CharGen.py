14b39d93fefbe9f3191d7bf39b8a8102c7e7bd84
morleidr <55928242+johnobrute@users.noreply.github.com>
2020-06-02 12:52:49 -0400

    Added some if statments

--- CharGen.py
+++ CharGen.py
@@ -9,23 +9,64 @@ By Morleidr
 import random
 from character_data import *
 
-##import importlib
-##
-##importlib.import_module('character_data')
 
+#   Determine Gender
 genders = ['Male','Female']
-g_count = len(genders)
-g_roll = random.randint(1,g_count)-1
+g_roll = random.randint(1,len(genders))-1
 gender = genders[g_roll]
 
-print(gender)
 
+#   Determine Race
+r_roll = random.randint(1,len(races))-1
+race = races[r_roll]
+
+
+#   Determine Name
+#   Based on Race
 if gender == 'Male':
-    lf_roll = random.randint(1,len(m_LizardfolkNames))-1
-    print("Name: ", m_LizardfolkNames[lf_roll])
+    if race == 'Human':
+        # need more if statements for human race
+        print('Human', gender)
+    elif race == 'Fey':
+        print('Fey', gender)
+    elif race == 'Dwarf':
+        print('Dwarf', gender)
+    elif race == 'Goblin':
+        print('Goblin', gender)
+    elif race == 'Salimar':
+        print('Salimar', gender)
+    elif race == 'Treefolk':
+        print('Treefolk', gender)
+    elif race == 'Karhu':
+        print('Karhu', gender)
+    else:
+        print('Lizardfolk', gender)
+    
+        
+
+    
+##    lf_roll = random.randint(1,len(m_LizardfolkNames))-1
+##    print("Name: ", m_LizardfolkNames[lf_roll])
 else:
-    lf_roll = random.randint(1,len(f_LizardfolkNames))-1
-    print(f_LizardfolkNames[lf_roll])
+    if race == 'Human':
+        # need more if statements for human race
+        print('Human', gender)
+    elif race == 'Fey':
+        print('Fey', gender)
+    elif race == 'Dwarf':
+        print('Dwarf', gender)
+    elif race == 'Goblin':
+        print('Goblin', gender)
+    elif race == 'Salimar':
+        print('Salimar', gender)
+    elif race == 'Treefolk':
+        print('Treefolk', gender)
+    elif race == 'Karhu':
+        print('Karhu', gender)
+    else:
+        print('Lizardfolk', gender)
+##    lf_roll = random.randint(1,len(f_LizardfolkNames))-1
+##    print(f_LizardfolkNames[lf_roll])
 
     
 
