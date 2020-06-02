499f8d13d6363c700e87b8caba356d11d89d3a71
morleidr <55928242+johnobrute@users.noreply.github.com>
2020-06-02 14:19:05 -0400

    Added human races

--- CharGen.py
+++ CharGen.py
@@ -23,48 +23,68 @@ race = races[r_roll]
 
 #   Determine Name
 #   Based on Race
+
+#   Male Character
 if gender == 'Male':
+    
     if race == 'Human':
+        hr_roll = random.randint(1,len(human_races))-1
+        human_race = human_races[hr_roll]
         # need more if statements for human race
-        print('Human', gender)
+        print('Human', gender,">", human_race)
+        
     elif race == 'Fey':
         print('Fey', gender)
+        
     elif race == 'Dwarf':
         print('Dwarf', gender)
+        
     elif race == 'Goblin':
         print('Goblin', gender)
+        
     elif race == 'Salimar':
         print('Salimar', gender)
+        
     elif race == 'Treefolk':
         print('Treefolk', gender)
+        
     elif race == 'Karhu':
         print('Karhu', gender)
+        
     else:
         print('Lizardfolk', gender)
-    
-        
+     
 
     
-##    lf_roll = random.randint(1,len(m_LizardfolkNames))-1
-##    print("Name: ", m_LizardfolkNames[lf_roll])
+#   Female Character
 else:
     if race == 'Human':
+        hr_roll = random.randint(1,len(human_races))-1
+        human_race = human_races[hr_roll]
         # need more if statements for human race
-        print('Human', gender)
+        print('Human', gender,">", human_race)
+        
     elif race == 'Fey':
         print('Fey', gender)
+        
     elif race == 'Dwarf':
         print('Dwarf', gender)
+        
     elif race == 'Goblin':
         print('Goblin', gender)
+        
     elif race == 'Salimar':
         print('Salimar', gender)
+        
     elif race == 'Treefolk':
         print('Treefolk', gender)
+        
     elif race == 'Karhu':
         print('Karhu', gender)
+        
     else:
         print('Lizardfolk', gender)
+        
 ##    lf_roll = random.randint(1,len(f_LizardfolkNames))-1
 ##    print(f_LizardfolkNames[lf_roll])
 
